63313d3517fc5162ebf51be5b5490cd5b1d1da18
NDanielR <n.danielramirez.h@gmail.com>
2024-08-08 13:46:40 -0500

    agrego GUI de movimiento de objeto

---
+++ Aguadulce/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Aguadulce
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void Coordinate_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (int.TryParse(coordenadaX.Text, out int x) && int.TryParse(coordenadaY.Text, out int y))
+            {
+                Canvas.SetLeft(rectangulo, x);
+                Canvas.SetTop(rectangulo, y);
+            }
+            else
+            {
+                Canvas.SetLeft(rectangulo, 0);
+                Canvas.SetTop(rectangulo, 0);
+            }
+        }
+ 
+
+    }
+}
